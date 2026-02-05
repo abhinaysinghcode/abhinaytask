5eb0a3828b3f80b8ab16214164d56aa7dc998cdf
abhinaysinghcode <abhinaytutedude@gmail.com>
2026-02-05 17:24:42 +0530

    Update assignment1task1.py

---
+++ assignment1task1.py
@@ -0,0 +1,16 @@
+#Task 1 
+
+a=int(input("Enter the first number: "))
+b=int(input("Enter the second number: "))
+
+#addition 
+print("Addition: ",a + b)
+
+# subtraction
+print("Subtraction: ",a - b)
+
+#Multiplication 
+print("Multiplication: ",a * b)
+
+#Division
+print("Division: ",a / b)
