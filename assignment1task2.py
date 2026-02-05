c821e39291e0ace788b489c3b75640c2c0f29055
abhinaysinghcode <abhinaytutedude@gmail.com>
2026-02-05 17:25:23 +0530

    Update assignment1task2.py

---
+++ assignment1task2.py
@@ -0,0 +1,7 @@
+# task 2
+
+first_name = str(input("Enter your first name: "))   # first name of user
+last_name = str(input("Enter your last name: "))     # last name of user
+
+# the greeting
+print(f"Hello, {first_name} {last_name}! Welcome to the Python program")
